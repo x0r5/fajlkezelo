aaa7dae60ddf94499e9d2bb933f36f064d34212a
Sebestyén Soma <obivan05@gmail.com>
2018-11-20 23:55:54 +0100

    kezdetek

---
+++ src/build/FilesData.java
@@ -0,0 +1,16 @@
+package build;
+
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+import java.util.AbstractMap;
+
+public class FilesData extends DefaultTreeModel {
+
+    public FilesData(TreeNode root) {
+        super(root);
+    }
+
+
+
+}
+
